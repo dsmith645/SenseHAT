39699799d0ab1d5adee8b1f6899f4461ad8cbe08
Dominic Smith <dsmith645@lsr7.net>
2018-12-17 12:58:45 -0600

    Added most of the color stuff and picking from it randomly

--- pong.py
+++ pong.py
@@ -1,14 +1,13 @@
 from sense_hat import SenseHat
 from time import sleep
+import random
 from random import randint
 sense = SenseHat()
 sense.clear()
 white = (255,255,255)
-blue = (0,0,255)
 red = (255,0,0)
-green = (0,255,0)
-purple = (255,255,0)
-cyan = (0,255,255)
+ball_colors = {1 : (0,0,255), 2 : (255,0,0), 3 : (0,255,0), 4 : (255,0,255),
+               5 : (0,255,255), 6 : (0,255,255)}
 bat_y = 4
 ball_position = [3,3]
 ball_velocity = [1,1]
@@ -28,12 +27,13 @@ def move_down(event):
         bat_y += 1
 
 def draw_ball():
-    sense.set_pixel(ball_position[0], ball_position[1], blue)
+    sense.set_pixel(ball_position[0], ball_position[1], ball_colors[1])
     ball_position[0] += ball_velocity[0]
     if ball_position[0] == 7 or ball_position[0] == 0:
         ball_velocity[0] = -ball_velocity[0]
+        sense.set_pixel(ball_position[0], ball_position[1], ball_colors[random.randint(1,6)])
     ball_position[1] += ball_velocity[1]
-    if ball_position[1] == 7 or ball_position[1] ==0:
+    if ball_position[1] == 7 or ball_position[1] == 0:
         ball_velocity[1] = -ball_velocity[1]
     if ball_position[0] == 1 and (bat_y - 1) <= ball_position[1] <= (bat_y + 1):
         ball_velocity[0] = -ball_velocity[0]
@@ -47,7 +47,7 @@ while True:
     sense.stick.direction_down = move_down
     if ball_position[0] == 0:
         sleep(1)
-        sense.set_pixel(ball_position[0], ball_position[1], blue)
+        sense.set_pixel(ball_position[0], ball_position[1], ball_colors[random.randint(1,6)])
         sense.clear(red)
         sleep(0.25)
         sense.clear()
